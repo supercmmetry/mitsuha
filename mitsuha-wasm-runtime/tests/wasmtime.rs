887176b4257ee1cc41bfe233445699f524b8797c
Vishaal Selvaraj <vselvaraj@microsoft.com>
2023-02-17 16:03:20 +0530

    fix(test): Fix makefiles

--- mitsuha-wasm-runtime/tests/wasmtime.rs
+++ mitsuha-wasm-runtime/tests/wasmtime.rs
@@ -30,9 +30,9 @@ impl Provider for InMemoryWasmProvider {
 
 impl InMemoryWasmProvider {
     pub fn new() -> Self {
-        let wasm_echo: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/mitsuha-wasm-echo/target/wasm32-unknown-unknown/release/mitsuha_wasm_echo.wasm").to_vec();
-        let wasm_loop: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/mitsuha-wasm-loop/target/wasm32-unknown-unknown/release/mitsuha_wasm_loop.wasm").to_vec();
-        let wasm_main: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/mitsuha-wasm-main/target/wasm32-unknown-unknown/release/mitsuha_wasm_main.wasm").to_vec();
+        let wasm_echo: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/target/wasm32-unknown-unknown/release/mitsuha_wasm_echo.wasm").to_vec();
+        let wasm_loop: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/target/wasm32-unknown-unknown/release/mitsuha_wasm_loop.wasm").to_vec();
+        let wasm_main: Vec<u8> = include_bytes!("../../mitsuha-runtime-test/target/wasm32-unknown-unknown/release/mitsuha_wasm_main.wasm").to_vec();
 
         Self {
             wasm_echo,
